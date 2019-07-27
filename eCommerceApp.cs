19b7b2aa6246176d5b9066c1458bf85d84425529
ngaisteve1 <findingsteve@gmail.com>
2019-07-27 10:24:16 -1200

    updated

--- eCommerceApp.cs
+++ eCommerceApp.cs
@@ -8,8 +8,77 @@ public class eCommerce
 {
     private const decimal deliverCharges = 10.00M;
     private List<Customer> validCustomers;
+    private List<OrderDetails> shoppingCart;
+    private List<Product> productList;
 
     public eCommerce()
+    {
+        InitializeCustomerList();
+        InitializeProductList();
+        shoppingCart = new List<OrderDetails>();
+    }
+
+    public void Execute()
+    {
+        var valid_Customer = Login();
+
+        while (true)
+        {
+            Console.Clear();
+            Console.WriteLine($"Login as {valid_Customer.FullName}");
+            Console.WriteLine("\nAzada E-Commerce App");
+            Console.WriteLine("1. Browse all products");
+            Console.WriteLine("2. View shopping cart");
+            Console.WriteLine("3. Logout");
+            Console.Write("Enter your option: ");
+            string opt = Console.ReadLine();
+            switch (opt)
+            {
+                case "1":
+                    BrowseProducts();
+                    AddShoppingCart(valid_Customer);
+                    break;
+                case "2":
+                    ViewShoppingCart(valid_Customer);
+                    break;
+                case "3":
+                    valid_Customer = null;
+                    shoppingCart.Clear();
+                    Execute();
+                    break;
+                default:
+                    Console.WriteLine("Invalid option.");
+                    break;
+            }
+            Console.ReadKey();
+        }
+    }
+
+    public Customer Login()
+    {
+        while (true)
+        {
+            var userInput = new Customer();
+            Console.Write("Username: ");
+            userInput.Username = Console.ReadLine();
+
+            Console.Write("Password: ");
+            userInput.Password = Console.ReadLine();
+
+            var validCustomer = validCustomers
+            .Where(c => c.Username.Equals(userInput.Username))
+            .Where(c => c.Password.Equals(userInput.Password))
+            .FirstOrDefault();
+
+            if (validCustomer != null)
+                return validCustomer;
+
+            Console.WriteLine("Invalid username or password");
+            Console.ReadKey();
+        }
+    }
+
+    private void InitializeCustomerList()
     {
         validCustomers = new List<Customer>(){
             new Customer(){
@@ -30,141 +99,105 @@ public class eCommerce
                 }
         };
     }
-    public void Execute()
+
+    private void InitializeProductList()
     {
-        var valid_Customer = Login();
-        if (valid_Customer == null)
+        productList = new List<Product>() {
+            new Product(){ Id = 2, ProductName = "MSI Laptop GL63 8RCS 058MY", UnitPrice = 2999.00M},
+            new Product(){ Id = 5, ProductName = "MSI Laptop GL63 8RC 057MY", UnitPrice = 3799.00M},
+            new Product(){ Id = 12, ProductName = "Logitech M330 Wireless Mouse", UnitPrice = 59.00M},
+            new Product(){ Id = 13, ProductName = "Mechanical Keyboard with Backlight", UnitPrice = 160.00M},
+            new Product(){ Id = 16, ProductName = "Dell Wireless Keyboard and Mouse Combo ", UnitPrice = 70.00M}
+        };
+    }
+
+    private void BrowseProducts()
+    {
+        var table = new ConsoleTable("Product Id", "Product Name", "Unit Price");
+
+        foreach (var product in productList)
+            table.AddRow(product.Id, product.ProductName, product.UnitPrice);
+
+
+        table.Write();
+    }
+
+    private void AddShoppingCart(Customer valid_Customer)
+    {
+        Console.Write("Enter the product ID you want to buy: ");
+        int productId = int.Parse(Console.ReadLine());
+
+        var selectedProduct = productList.FirstOrDefault(p => p.Id == productId);
+
+        if (selectedProduct == null)
         {
-            Console.WriteLine("Invalid username or password.");
-            Console.ReadKey();
+            Console.WriteLine("Product not found.");
             return;
         }
 
-        var shoppingCart = new List<OrderDetails>();
-        var productList = new List<Product>();
-        while (true)
-        {
-            Console.Clear();
-            Console.WriteLine("Azada E-Commerce App");
-            Console.WriteLine("1. Browser all products.");
-            Console.WriteLine("2. View shopping cart.");
-            Console.Write("Enter your option: ");
-            string opt = Console.ReadLine();
-            switch (opt)
-            {
-                case "1":
-                    productList = new List<Product>() {
-                        new Product(){ Id = 2, ProductName = "MSI Laptop GL63 8RCS 058MY", UnitPrice = 2999.00M},
-                        new Product(){ Id = 5, ProductName = "MSI Laptop GL63 8RC 057MY", UnitPrice = 3799.00M},
-                        new Product(){ Id = 12, ProductName = "Logitech M330 Wireless Mouse", UnitPrice = 59.00M},
-                        new Product(){ Id = 13, ProductName = "Mechanical Keyboard with Backlight", UnitPrice = 160.00M},
-                        new Product(){ Id = 16, ProductName = "Dell Wireless Keyboard and Mouse Combo ", UnitPrice = 70.00M}
-                    };
+        Console.Write("Enter quantity to buy: ");
+        int quantity = int.Parse(Console.ReadLine());
 
-                    var table = new ConsoleTable("Product Id", "Product Name", "Unit Price");
+        var order = new OrderDetails();
+        order.Customer_Id = valid_Customer.CustomerId;
+        order.ProductId = productId;
+        order.QuantityOrder = quantity;
+        order.TotalAmount = quantity * selectedProduct.UnitPrice;
 
-                    foreach (var product in productList)
-                        table.AddRow(product.Id, product.ProductName, product.UnitPrice);
-
-
-                    table.Write();
-
-                    Console.Write("Enter the product ID you want to buy: ");
-                    int productId = int.Parse(Console.ReadLine());
-
-                    var selectedProduct = productList.FirstOrDefault(p => p.Id == productId);
-
-                    if (selectedProduct == null)
-                    {
-                        Console.WriteLine("Product not found.");
-                        break;
-                    }
-
-                    Console.Write("Enter quantity to buy: ");
-                    int quantity = int.Parse(Console.ReadLine());
-
-                    var order = new OrderDetails();
-                    order.Customer_Id = valid_Customer.CustomerId;
-                    order.ProductId = productId;
-                    order.QuantityOrder = quantity;
-                    order.TotalAmount = quantity * selectedProduct.UnitPrice;
-
-                    shoppingCart.Add(order);
-                    Console.WriteLine($"{selectedProduct.ProductName} added into shopping cart.");
-
-                    break;
-                case "2":
-                    // inner join orderdetail and product.
-                    var shoppingCart2 = from s in shoppingCart
-                                        join p in productList on s.ProductId equals p.Id
-                                        select new { p.ProductName, p.UnitPrice, s.QuantityOrder, s.TotalAmount };
-                    var totalOrderAmount = 0M;
-                    totalOrderAmount = shoppingCart.Sum(s => s.TotalAmount);
-                    var table2 = new ConsoleTable("Product Name", "Price", "Quantity", "Total");
-
-                    if (shoppingCart2.ToList().Count == 0)
-                    {
-                        System.Console.WriteLine("Shopping cart is empty. Go to browse products.");
-                        break;
-                    }
-
-                    foreach (var item in shoppingCart2)
-                        table2.AddRow(item.ProductName, item.UnitPrice, item.QuantityOrder, item.TotalAmount);
-
-                    table2.Write();
-
-                    Console.WriteLine("------------------------------------------");
-                    Console.WriteLine("Delivery charges: " + deliverCharges);
-                    Console.WriteLine("Total Order Amount: " + (totalOrderAmount + deliverCharges));
-                    Console.WriteLine("------------------------------------------");
-                    Console.WriteLine("Delivery Address: " + valid_Customer.Address);
-
-                    Console.WriteLine("\n1. Confirm order and proceed to make payment.");
-                    Console.WriteLine("2. Cancel order.");
-                    Console.Write("Enter option:");
-                    string opt2 = Console.ReadLine();
-                    switch (opt2)
-                    {
-                        case "1":
-                            Console.WriteLine("\nChoose payment method:");
-                            Console.WriteLine("1. Cash on delivery (COD)");
-                            Console.WriteLine("2. Credit Card");
-                            Console.WriteLine("3. Online eBanking");
-                            Console.ReadKey();
-                            Console.WriteLine("Info: This E-Commerce simulation app ends here....");
-                            break;
-                        case "2":
-                            shoppingCart.Clear();
-                            System.Console.WriteLine("Shopping cart is empty. Go to browse products.");
-                            break;
-                        default:
-                            Console.WriteLine("Invalid option.");
-                            break;
-                    }
-
-                    break;
-                default:
-                    Console.WriteLine("Invalid option.");
-                    break;
-            }
-            Console.ReadKey();
-        }
+        shoppingCart.Add(order);
+        Console.WriteLine($"{selectedProduct.ProductName} added into shopping cart.");
     }
 
-    public Customer Login()
+    private void ViewShoppingCart(Customer valid_Customer)
     {
-        var userInput = new Customer();
-        Console.Write("Username: ");
-        userInput.Username = Console.ReadLine();
+        // inner join orderdetail and product.
+        var shoppingCart2 = from s in shoppingCart
+                            join p in productList on s.ProductId equals p.Id
+                            select new { p.ProductName, p.UnitPrice, s.QuantityOrder, s.TotalAmount };
+        var totalOrderAmount = 0M;
+        totalOrderAmount = shoppingCart.Sum(s => s.TotalAmount);
+        var table2 = new ConsoleTable("Product Name", "Price", "Quantity", "Total");
 
-        Console.Write("Password: ");
-        userInput.Password = Console.ReadLine();
+        if (shoppingCart2.ToList().Count == 0)
+        {
+            System.Console.WriteLine("Shopping cart is empty. Go to browse products.");
+            return;
+        }
 
-        var validCustomer = validCustomers
-        .Where(c => c.Username.Equals(userInput.Username))
-        .Where(c => c.Password.Equals(userInput.Password))
-        .FirstOrDefault();
+        foreach (var item in shoppingCart2)
+            table2.AddRow(item.ProductName, item.UnitPrice, item.QuantityOrder, item.TotalAmount);
 
-        return validCustomer;
+        table2.Write();
+
+        Console.WriteLine("------------------------------------------");
+        Console.WriteLine("Delivery charges: " + deliverCharges);
+        Console.WriteLine("Total Order Amount: " + (totalOrderAmount + deliverCharges));
+        Console.WriteLine("------------------------------------------");
+        Console.WriteLine("Delivery Address: " + valid_Customer.Address);
+
+        Console.WriteLine("\n1. Confirm order and proceed to make payment.");
+        Console.WriteLine("2. Cancel order.");
+        Console.Write("Enter option: ");
+        string opt2 = Console.ReadLine();
+        switch (opt2)
+        {
+            case "1":
+                Console.WriteLine("\nChoose payment method:");
+                Console.WriteLine("1. Cash on delivery (COD)");
+                Console.WriteLine("2. Credit Card");
+                Console.WriteLine("3. Online eBanking");
+                Console.Write("Enter option: ");
+                Console.ReadKey();
+                Console.WriteLine("Info: This E-Commerce simulation app ends here....");
+                break;
+            case "2":
+                shoppingCart.Clear();
+                System.Console.WriteLine("Shopping cart is empty. Go to browse products.");
+                break;
+            default:
+                Console.WriteLine("Invalid option.");
+                break;
+        }
+
     }
 }
